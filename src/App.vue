a49564f60c784307f9cd01b41f5a3ad16b41f628
李继龙 <kljzndx@outlook.com>
2024-09-29 08:41:57 +0800

    完成link组件制作

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import MyLink from './components/myLink.vue';
 
 </script>
 
 <template>
   <div>
-
+    <MyLink link="#" text="测试"></MyLink>
   </div>
 </template>
 
---
+++ src/components/myLink.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props=defineProps<{
+    link:string,
+    text:string,
+}>();
+</script>
+<template>
+    <div>
+        <a class="my-link" :href="props.link">{{ props.text }}</a>
+    </div>
+</template>
+<style scoped>
+.my-link{
+    display: block;
+    width: max-content;
+    padding: 0.5rem 0.75rem;
+    background-color: hsl(0, 0%, 90%);
+    text-decoration: none;
+    color: black;
+}
+.my-link:hover{
+    background-color: hsl(0, 0%, 80%);
+}
+</style>
